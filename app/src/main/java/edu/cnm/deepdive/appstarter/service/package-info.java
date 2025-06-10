f5da319594dc297455018168f032462104dda9dd
tmomonet <87035520+tmomonet@users.noreply.github.com>
2025-06-10 10:34:17 -0600

    Initial commit

---
+++ app/src/main/java/edu/cnm/deepdive/appstarter/service/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Repository, DAO, and service classes providing access to the app's persistent data store, device &amp; external services, permissions
+ * &amp; preferences, etc.
+ */
+package edu.cnm.deepdive.appstarter.service;
