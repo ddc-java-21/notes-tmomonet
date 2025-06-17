22a9c3cfad310ccc3b69937910e781f2f444f381
tmomonet <thollins@cnm.edu>
2025-06-17 16:50:57 -0600

    Handling permissions

--- app/src/main/java/edu/cnm/deepdive/notes/controller/DetailsFragment.java
+++ app/src/main/java/edu/cnm/deepdive/notes/controller/DetailsFragment.java
@@ -1,26 +1,44 @@
 package edu.cnm.deepdive.notes.controller;
 
+import android.Manifest;
+import android.Manifest.permission;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.ViewModelProvider;
+import com.google.android.material.snackbar.Snackbar;
+import edu.cnm.deepdive.notes.R;
 import edu.cnm.deepdive.notes.databinding.FragmentDetailsBinding;
 import edu.cnm.deepdive.notes.model.pojo.NoteWithImages;
 import edu.cnm.deepdive.notes.viewmodel.NoteViewModel;
 
 public class DetailsFragment extends Fragment {
 
-  // TODO: 6/17/2025 Define binding instance
-
   private FragmentDetailsBinding binding;
   private NoteViewModel viewModel;
   private long noteId;
   private NoteWithImages note;
+
+  private final ActivityResultLauncher<String> requestCameraPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), (granted) ->{
+    if (granted) {
+      // TODO: 6/17/2025 Make camera capture control visible
+    }
+    else {
+      // TODO: 6/17/2025 Make camera capture control GONE
+    }
+  });
+
+
 
   @Override
   public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -52,11 +70,41 @@ public class DetailsFragment extends Fragment {
     else {
       note = new NoteWithImages();
     }
+    checkCameraPermission();
   }
 
   @Override
   public void onDestroyView() {
     binding = null;
     super.onDestroyView();
+  }
+
+  private void checkCameraPermission(){
+    if (!hasCameraPermission()) {
+      if (shouldExplainCameraPermission()){
+        explainCameraPermission();
+      }
+    } else{
+      requestCameraPermission();
+    }
+  }
+
+  private void requestCameraPermission() {
+    requestCameraPermissionLauncher.launch(permission.CAMERA);
+  }
+
+  private boolean hasCameraPermission(){
+    return ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.CAMERA)
+        == PackageManager.PERMISSION_GRANTED;
+  }
+
+  private boolean shouldExplainCameraPermission() {
+    return ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), Manifest.permission.CAMERA);
+  }
+
+  private void explainCameraPermission() {
+    Snackbar.make(binding.getRoot(), R.string.camera_permission_explanation, Snackbar.LENGTH_INDEFINITE)
+        .setAction(android.R.string.ok, (v) -> requestCameraPermission())
+        .show();
   }
 }
